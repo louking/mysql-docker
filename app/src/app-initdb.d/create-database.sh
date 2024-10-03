3baa567215fe390212f84f835c164f8df893eb2e
Lou King <lking@pobox.com>
2024-10-03 06:35:12 -0400

    load initial SQL file from $DB_INIT_DIR

--- app/src/app-initdb.d/create-database.sh
+++ app/src/app-initdb.d/create-database.sh
@@ -3,24 +3,7 @@
 # NOTE: file_env, docker_process_sql, _mysql_passfile, logging functions COPIED from 
 # https://github.com/docker-library/mysql/blob/master/8.0/docker-entrypoint.sh
 
-# logging functions
-mysql_log() {
-	local type="$1"; shift
-	# accept argument string or stdin
-	local text="$*"; if [ "$#" -eq 0 ]; then text="$(cat)"; fi
-	local dt; dt="$(date -Iseconds)"
-	printf '%s [%s] [Entrypoint]: %s\n' "$dt" "$type" "$text"
-}
-mysql_note() {
-	mysql_log Note "$@"
-}
-mysql_warn() {
-	mysql_log Warn "$@" >&2
-}
-mysql_error() {
-	mysql_log ERROR "$@" >&2
-	exit 1
-}
+source ./app-initdb.d/sql-commands.sh
 
 # usage: file_env VAR [DEFAULT]
 #    ie: file_env 'XYZ_DB_PASSWORD' 'example'
@@ -41,25 +24,6 @@ file_env() {
 	fi
 	export "$var"="$val"
 	unset "$fileVar"
-}
-
-_mysql_passfile() {
-	# echo the password to the "file" the client uses
-	# the client command will use process substitution to create a file on the fly
-	# ie: --defaults-extra-file=<( _mysql_passfile )
-    cat <<-EOF
-        [client]
-        password="`cat /run/secrets/root-password`"
-	EOF
-    # note use of tab character above
-}
-
-# Execute sql script, passed via stdin
-#    ie: docker_process_sql <<<'INSERT ...'
-#    ie: docker_process_sql <my-file.sql
-docker_process_sql() {
-    # default mysql but caller can override
-	mysql --defaults-extra-file=<( _mysql_passfile) -uroot -hdb --comments --database=mysql "$@"
 }
 
 # follow pattern for each application database (https://stackoverflow.com/a/68714439/799921)
--- app/src/dbupgrade_and_run.sh
+++ app/src/dbupgrade_and_run.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+
+source ./app-initdb.d/sql-commands.sh
 
 # NOTE: file end of line characters must be LF, not CRLF (see https://stackoverflow.com/a/58220487/799921)
 
@@ -8,9 +10,16 @@ do
     sleep 5
 done
 
+# look for users sql file, should only be one, delete after loading sql into database
+files=(/initdb.d/users-*.sql)
+[ -f "$files" ] && ((${#files[@]}==1)) && docker_process_sql --database=users <$files && rm $files
+
 # initial volume create may cause flask db upgrade to fail
-while ! flask db upgrade
-do
-    sleep 5
-done
+# while ! flask db upgrade
+# do
+#     sleep 5
+# done
+
+flask db upgrade
+
 exec "$@"
