49532930d3b1b0b22deabe29a2d166cd03b38d7a
Lou King <lking@pobox.com>
2024-10-24 14:28:28 -0400

    deployment / versioning (louking/members#591)

--- app/src/mysql_docker/version.py
+++ app/src/mysql_docker/version.py
@@ -1,5 +1,6 @@
-# See https://packaging.python.org/guides/distributing-packages-using-setuptools/#semantic-versioning-preferred
-__version__ = '0.0.1.dev1'
+from os import environ
+
+__version__ = environ['APP_VER']    # from .env
 __docversion__ = __version__
 # uncomment for development
-__docversion__ = 'latest'
+# __docversion__ = 'latest'
